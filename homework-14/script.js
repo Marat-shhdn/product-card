94339cbba42732d63995aefa508d4243684b7931
Marat-shhdn <sheihutd@gmail.com>
2026-09-23 15:15:21 +0300

    (homework №14):fixed mistake

---
+++ homework-14/script.js
@@ -0,0 +1,9 @@
+import { Modal_2 } from "./modal.js";
+
+const registrationModal = new Modal_2(
+  "registration-modal",
+  "registration-button",
+  true,
+);
+
+const authModal = new Modal_2("auth-modal", "auth-button", false);
--- script.js
+++
@@ -1,9 +0,0 @@
-import { Modal_2 } from "./homework-14.js";
-
-const registrationModal = new Modal(
-  "registration-modal",
-  "registration-button",
-  true,
-);
-
-const authModal = new Modal("auth-modal", "auth-button", false);
